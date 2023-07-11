3fa577f399f2b84bceb733be7b05ba5670c65240
JuangarcDev <juangarciagrr@gmail.com>
2023-07-11 01:01:00 -0500

    Se configura y prueba el metodo get, post y put para paciente

--- servidor/controllers/pacienteController.js
+++ servidor/controllers/pacienteController.js
@@ -18,3 +18,38 @@ exports.crearPaciente = async (req, res) => {
     }
 
 }
+
+exports.obtenerPaciente = async (req, res) => {
+    try {
+        
+        const paciente = await Paciente.find();
+        res.json(paciente)
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error");
+    }
+}
+
+exports.actualizarPaciente =async (req, res) => {
+
+    try {
+        const { nombre, apellido, fechaNacimiento, edad} = req.body;
+        let paciente = await Paciente.findById(req.params.id);
+
+        if(!paciente) {
+            res.status(404).json({ msg: 'No existe el paciente solicitado' })
+        }
+
+        paciente.nombre = nombre;
+        paciente.apellido = apellido;
+        paciente.fechaNacimiento =fechaNacimiento;
+        paciente.edad =edad;
+        
+        paciente =await Paciente.findOneAndUpdate({ _id: req.params.id}, paciente,{new: true} )
+        res.json(paciente);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('hubo un error');
+    }
+}
--- servidor/routes/paciente.js
+++ servidor/routes/paciente.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const pacienteController = require('../controllers/pacienteController');
 
 // api/paciente
-router.post('/', pacienteController.crearPaciente)
-
+router.post('/', pacienteController.crearPaciente);
+router.get('/', pacienteController.obtenerPaciente);
+router.put('/:id', pacienteController.actualizarPaciente)
 module.exports = router;
