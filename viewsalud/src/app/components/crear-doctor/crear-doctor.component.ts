a04e5e1ae9822a09d5351a03b9e6493f2b4028f4
JuangarcDev <juangarciagrr@gmail.com>
2023-07-13 16:39:19 -0500

    Se crean los validadores del formulario de crear doctor

--- viewsalud/src/app/components/crear-doctor/crear-doctor.component.ts
+++ viewsalud/src/app/components/crear-doctor/crear-doctor.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 
 
@@ -9,6 +10,24 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './crear-doctor.component.html',
   styleUrls: ['./crear-doctor.component.css']
 })
-export class CrearDoctorComponent {
+export class CrearDoctorComponent implements OnInit {
+  doctorForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.doctorForm = this.fb.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      especialidad: ['', Validators.required],
+      consultorio: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/), Validators.min(1)]],
+      correo: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  agregarDoctor(){
+    console.log(this.doctorForm)
+  }
 }
