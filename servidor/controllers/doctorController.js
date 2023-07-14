9350e2dbf6141220fea525a7e802ab8459991647
JuangarcDev <juangarciagrr@gmail.com>
2023-07-14 01:53:25 -0500

    Se crean rutas y metodos para editar y eliminar doctores

--- servidor/controllers/doctorController.js
+++ servidor/controllers/doctorController.js
@@ -28,3 +28,57 @@ exports.obtenerDoctores = async (req, res) =>  {
         res.status(500).send("Hubo un error ..");
     }
 }
+
+exports.actualizarDoctor = async (req, res) => {
+    try {
+        const {nombre, apellido, especialidad, consultorio, correo} = req.body;
+        let doctor = await Doctor.findById(req.params.id);
+
+        if(!doctor) {
+            res.status(404).json({ msg: "No existe el doctor"})
+        }
+
+        doctor.nombre = nombre;
+        doctor.apellido = apellido;
+        doctor.especialidad = especialidad;
+        doctor.consultorio = consultorio;
+        doctor.correo = correo;
+
+        doctor = await Doctor.findOneAndUpdate({ _id: req.params.id}, doctor, { new: true})
+        res.json(doctor);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error ..");
+    }
+}
+
+exports.obtenerDoctor = async (req, res) => {
+    try {
+        let doctor = await Doctor.findById(req.params.id);
+
+        if(!doctor) {
+            res.status(404).json({ msg: "No existe el doctor"})
+        }
+
+        res.json(doctor);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error ..");
+    }
+}
+
+exports.eliminarDoctor = async (req, res) => {
+    try {
+        let doctor = await Doctor.findById(req.params.id);
+
+        if(!doctor) {
+            res.status(404).json({ msg: "No existe el doctor"})
+        }
+
+        await Doctor.findOneAndRemove({ _id: req.params.id })
+        res.json({ msg: 'El doctor fue eliminado con exito'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Hubo un error ..");
+    }
+}
--- servidor/routes/doctor.js
+++ servidor/routes/doctor.js
@@ -6,5 +6,8 @@ const doctorController = require('../controllers/doctorController');
 //se configura ruta para api/doctor
 router.post('/', doctorController.crearDoctor);
 router.get('/', doctorController.obtenerDoctores);
+router.put('/:id', doctorController.actualizarDoctor);
+router.get('/:id', doctorController.obtenerDoctor);
+router.delete('/:id', doctorController.eliminarDoctor);
 
 module.exports = router;
