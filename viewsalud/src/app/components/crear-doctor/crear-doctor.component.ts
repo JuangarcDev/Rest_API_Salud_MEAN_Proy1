8d4f0b2cd0973c7b775bf8c6ae7a17137652da59
JuangarcDev <juangarciagrr@gmail.com>
2023-07-14 13:14:41 -0500

    Se termina de configurar el editado de un doctor BD-Cliente

--- viewsalud/src/app/components/crear-doctor/crear-doctor.component.ts
+++ viewsalud/src/app/components/crear-doctor/crear-doctor.component.ts
@@ -3,8 +3,9 @@ import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Doctor } from 'src/app/models/doctor';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { DoctorService } from 'src/app/services/doctor.service';
 
 
 
@@ -15,10 +16,14 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class CrearDoctorComponent implements OnInit {
   doctorForm: FormGroup;
+  titulo = 'Agregar Doctor';
+  id: string | null;
 
   constructor(private fb: FormBuilder,
               private router: Router,
-              private toastr: ToastrService) {
+              private toastr: ToastrService,
+              private _doctorService: DoctorService,
+              private aRouter: ActivatedRoute) {
     this.doctorForm = this.fb.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
@@ -26,10 +31,11 @@ export class CrearDoctorComponent implements OnInit {
       consultorio: ['', [Validators.required, Validators.pattern(/^[1-9]\d*$/), Validators.min(1)]],
       correo: ['', [Validators.required, Validators.email]]
     })
+    this.id = this.aRouter.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
-    
+    this.esEditarDoctor();
   }
 
   agregarDoctor(){
@@ -42,8 +48,47 @@ export class CrearDoctorComponent implements OnInit {
       consultorio: this.doctorForm.get('consultorio')?.value,
       correo: this.doctorForm.get('correo')?.value,
     }
-    console.log(DOCTOR);
-    this.toastr.success('El Doctor fue registrado con exito ;)', 'Doctor Registrado');
-    this.router.navigate(['/listar-doctor']);
+
+    if(this.id !== null) {
+      // se edita el doctor existente
+      this._doctorService.editarDoctor(this.id, DOCTOR).subscribe(data =>{
+      this.toastr.info('El Doctor fue editado con exito ;)', 'Doctor Actualizado');
+      this.router.navigate(['/listar-doctor']);
+      }, error =>{
+        console.log(error);
+        this.toastr.error('El Doctor no pudo ser editado :(', 'Doctor no Actualizado');
+        this.doctorForm.reset();
+      })
+
+    } else{
+      // se agrega un nuevo doctor
+      console.log(DOCTOR);
+      this._doctorService.guardarDoctor(DOCTOR).subscribe(data =>{
+      this.toastr.success('El Doctor fue registrado con exito ;)', 'Doctor Registrado');
+      this.router.navigate(['/listar-doctor']);
+    }, error =>{
+      console.log(error);
+      this.toastr.error('El Doctor no pudo ser registrado :(', 'Doctor no Registrado');
+      this.doctorForm.reset();
+    })
+    }
+
+    
+
+  }
+
+  esEditarDoctor() {
+    if(this.id !== null){
+      this.titulo = 'Editar Doctor';
+      this._doctorService.obtenerDoctor(this.id).subscribe(data =>{
+        this.doctorForm.setValue({
+          nombre: data.nombre,
+          apellido: data.apellido,
+          especialidad: data.especialidad,
+          consultorio: data.consultorio,
+          correo: data.correo,
+        })
+      })
+    }
   }
 }
--- viewsalud/src/app/services/doctor.service.ts
+++ viewsalud/src/app/services/doctor.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Doctor } from '../models/doctor';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,24 @@ export class DoctorService {
   urldoctor = 'http://localhost:4000/api/doctor/';
 
   constructor(private http: HttpClient) { }
-
+//para listar
   getDoctores(): Observable<any> {
     return this.http.get(this.urldoctor);
   }
-
+//para eliminar
   eliminarDoctor(id: string): Observable<any> {
     return this.http.delete(this.urldoctor + id);
   }
+//para persistir
+  guardarDoctor(doctor: Doctor): Observable<any> {
+    return this.http.post(this.urldoctor, doctor);
+  }
+//para obtener el doctor a editar
+obtenerDoctor(id: string): Observable<any> {
+  return this.http.get(this.urldoctor + id);
+}
+//edita el doctor que se obtuvo anteriormente
+editarDoctor(id: string, doctor: Doctor): Observable<any> {
+  return this.http.put(this.urldoctor + id, doctor);
+}
 }
