8cafec166d4812b81eb4ef2af36be7aac63b3f67
JuangarcDev <juangarciagrr@gmail.com>
2023-07-14 12:26:56 -0500

    Se agrega la funcionalidad de listar los y eliminar doctores de la BD

--- viewsalud/src/app/components/listar-doctor/listar-doctor.component.ts
+++ viewsalud/src/app/components/listar-doctor/listar-doctor.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { DoctorService } from 'src/app/services/doctor.service';
+import { Doctor } from 'src/app/models/doctor';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Component({
@@ -8,6 +11,33 @@ import { FooterComponent } from '../footer/footer.component';
   templateUrl: './listar-doctor.component.html',
   styleUrls: ['./listar-doctor.component.css']
 })
-export class ListarDoctorComponent {
+export class ListarDoctorComponent implements OnInit {
+  listDoctores: Doctor[] = [];
 
+  constructor(private _doctorService: DoctorService,
+              private toastr: ToastrService) {
+
+  }
+
+  ngOnInit(): void {
+    this.obtenerDoctores();
+  }
+//Listar doctores
+  obtenerDoctores() {
+    this._doctorService.getDoctores().subscribe(data => {
+      console.log(data);
+      this.listDoctores = data;
+    }, error => {
+      console.log(error);
+    })
+  }
+//Elimina un doctor previamente creado
+  eliminarDoctor(id: any) {
+    this._doctorService.eliminarDoctor(id).subscribe(data =>{
+      this.toastr.error('El doctor fue eliminado exitosamente..','Doctor elimando');
+      this.obtenerDoctores();
+    }, error => {
+      this.toastr.error('Fallo en la eliminación del doctor','Doctor no fue elimando');
+    })
+  }
 }
--- viewsalud/src/app/services/doctor.service.ts
+++ viewsalud/src/app/services/doctor.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
+  urldoctor = 'http://localhost:4000/api/doctor/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDoctores(): Observable<any> {
+    return this.http.get(this.urldoctor);
+  }
+
+  eliminarDoctor(id: string): Observable<any> {
+    return this.http.delete(this.urldoctor + id);
+  }
 }
